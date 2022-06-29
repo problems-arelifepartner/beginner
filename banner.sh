47140f4f3d985db1dfeb76446a2c4c07df0c9d14
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-06-29 21:09:07 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,17 @@
+pkg update
+echo ""
+
+pkg upgrade
+echo ""
+
+pkg install git
+echo ""
+
+git clone https://github.com/RED5PY/redspy-banner.git
+echo ""
+
+cd redspy-banner
+echo ""
+
+bash boom.sh
+echo ""
